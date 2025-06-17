596762209658ade9b3f3a2bd51836a17863fadff
23f3001930 <23f3001930@ds.iitm.study.ac.in>
2025-06-17 23:15:21 +0530

    dsv

--- app.py
+++ app.py
@@ -736,5 +736,3 @@ async def proxy_to_query(request: Request):
 async def root():
     return {"message": " FastAPI app is deployed on Vercel and running successfully!"}
 
-if __name__ == "__main__":
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
